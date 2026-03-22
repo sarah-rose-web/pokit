b937d8d9034232a51b2e68f6865b05f38824ee84
Sarah Rose <srmlinas@gmail.com>
2026-03-22 09:27:31 +0800

    fix: hardcode App Check debug token for localhost

--- src/firebase/appcheck.js
+++ src/firebase/appcheck.js
@@ -5,7 +5,7 @@ import app from './config'
 // to get a debug token you can whitelist in the Firebase console.
 if (import.meta.env.DEV) {
   // @ts-ignore
-  self.FIREBASE_APPCHECK_DEBUG_TOKEN = true
+  self.FIREBASE_APPCHECK_DEBUG_TOKEN = '7418be43-6351-44b1-9d87-af6a994ce4d9'
 }
 
 initializeAppCheck(app, {
